7b80aab592804e7314b3e6add485d77abc2a388f
it3r4-ramos <winonajoesa@gmail.com>
2019-03-20 20:18:34 +0800

    my first commit

---
+++ application/views/templates/body.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GitRepo Winona</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php  echo base_url("assets/vendor/bootstrap/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css"); ?>">
+
+    <script type="text/javascript" href="<?php echo base_url("assets/js/style.js") ?>"></script>
+</head>
+<body>
+	<h1><b><center>GITHUB REPOSITORY</center></b></h1>
+	<br>
+	<br>
+	<h3><b><center>THIS IS MY FIRST REPOSITORY</center></b></h3>
+
+	<br>
+	<br>
+	<h5><b><center>BY: WINONA RAMOS</center></b></h5>
+</body>
+</html>
